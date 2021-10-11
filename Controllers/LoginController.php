8a20f0bab8c1f9d12ebb4a4356e018389d2d2251
Alejandro Bocchicchio <aleb2800@gmail.com>
2021-10-10 21:45:53 -0300

    adding code

--- Controllers/LoginController.php
+++ Controllers/LoginController.php
@@ -5,7 +5,27 @@
     {
         public function Login()
         {
-            
+            $parameters = array();
+            if ($_SERVER['REQUEST_METHOD'] == "POST") {
+                $parameters = $_POST;
+                $email = $_POST["user"];
+                $password = $_POST["password"];
+        
+
+                if (($email == "usuario") && ($password == "123"))
+                {
+                    session_start();
+                    $loggedUser = new Client();
+                    $loggedUser->setUserName($email);
+                    $loggedUser->setPassword($password);
+                    $_SESSION["loggedUser"] = $loggedUser;
+                    header("location:nav.php");
+                }
+            }
+            else
+            {
+                header("location:index.php");
+            }
         }
     }
 ?>
