86bb77a48feb7c47a06d2c3e3a6e59ccdbc0e2e0
Franco Gomez <alejo_no@hotmail.com>
2021-10-10 22:44:07 -0300

    adding files

--- Config/Config.php
+++ Config/Config.php
@@ -2,7 +2,11 @@
 
 define("ROOT", dirname(__DIR__) . "/");
 //Path to your project's root folder
+<<<<<<< HEAD
 define("FRONT_ROOT", "/tp-final-laboratorio-iv/");
+=======
+define("FRONT_ROOT", "/TP_Final/");
+>>>>>>> 479e4542380d2203e3b3678a85c0009714f88d50
 define("VIEWS_PATH", "Views/");
 define("CSS_PATH", FRONT_ROOT.VIEWS_PATH . "css/");
 define("JS_PATH", FRONT_ROOT.VIEWS_PATH . "js/");
--- Models/Person.php
+++ Models/Person.php
@@ -1,6 +1,7 @@
 <?php
 namespace Models;
 
+<<<<<<< HEAD
 class Person
 {
     private $firstName;
@@ -26,3 +27,76 @@ class Person
         $this->lastName = $lastName;
     }
 }
+=======
+    class Person
+    {
+        private string $firstName;
+        private string $lastName;
+        private int $dni;
+        private string $gender;
+        private DateTime $birthDate;
+        private string $email;
+        private string $phoneNumber;
+        private bool $active;
+
+        public function getFirstName(): string
+        {
+            return $this->firstName;
+        }
+
+        public function setFirstName($firstName): void
+        {
+            $this->firstName = $firstName;
+        }
+
+        public function getLastName(): string
+        {
+            return $this->lastName;
+        }
+
+        public function setLastName($lastName): void
+        {
+            $this->lastName = $lastName;
+        }
+
+        public function getDni(): string
+        {
+            return $this->dni;
+        }
+
+        public function setDni($dni): void
+        {
+            $this->dni = $dni;
+        }
+
+        public function getGender(): string
+        {
+            return $this->gender;
+        }
+
+        public function setGender($gender): void
+        {
+            $this->gender = $gender;
+        }
+
+        public function getBirthDate(): DateTime
+        {
+            return $this->birthDate;
+        }
+
+        public function setBirthDate($birthDate): void
+        {
+            $this->birthDate = $birthDate;
+        }
+
+        public function isActive(): bool
+        {
+            return $this->active;
+        }
+
+        public function setActive($active): void
+        {
+            $this->active = $active;
+        }
+    }
+>>>>>>> 479e4542380d2203e3b3678a85c0009714f88d50
