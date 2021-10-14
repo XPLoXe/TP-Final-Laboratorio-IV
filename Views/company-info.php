8f1d361130d3b7ab4c9e47860cbf684b0cb5771f
Alejandro Bocchicchio <aleb2800@gmail.com>
2021-10-14 20:40:38 -0300

    More company details added & session validations

--- Views/company-info.php
+++ Views/company-info.php
@@ -1,6 +1,9 @@
 <?php
   require_once('nav.php');
-  
+  if (!isset($_SESSION["loggedUser"])) 
+  {
+    require_once('login.php');
+  }
 ?>
 
 <!DOCTYPE html>
@@ -17,19 +20,22 @@
     <div>
       <h1><b><?php echo $company->getName() ?></b></h1>
       <img src="<?php echo FRONT_ROOT.IMG_PATH.$company->getLogo() ?>" style="width:75%; max-height:75%" > 
-      <h2 style="padding: 1%;"><b>Contact Info:</b></h2>
+      <br>
+      <br><h2 style="padding: 1%;"><b>Contact Info:</b></h2>
       <p>
         <img src="<?php echo FRONT_ROOT.IMG_PATH ?>tel.png">
-        <?php echo $company->getPhoneNumber(). "  "?>
+        <?php echo $company->getPhoneNumber(). ""?>
         <br>
         <img src="<?php echo FRONT_ROOT.IMG_PATH ?>email.png"> <?php echo $company->getEmail()?>
-        <?php echo $company->getPhoneNumber(). " --"?> 
       </p>
     </div>
     </div>
-    <div class="col">
+    <div class="col" style="font-size: x-large; text-align:center; margin-top: 35px">
       <br>
-      <p style="font-size: x-large; text-align:center; margin-top: 35px;"><?php echo $company->getDescription() ?></p>
+      <b>Fundada en:</b> <?php echo $company->getYearFoundation() ?> <br>
+      <b>Radicada en:</b> <?php echo $company->getCity() ?> <br> 
+      <br>
+      <p><strong><?php echo $company->getDescription() ?></strong></p>
     </div>
   </div>
 </div>
--- Views/student-list.php
+++ Views/student-list.php
@@ -1,5 +1,9 @@
 <?php
     require_once('nav.php');
+    if (!isset($_SESSION["loggedUser"])) 
+    {
+     require_once('login.php');
+    }
 ?>
 <main class="py-5">
      <section id="listado" class="mb-5">
